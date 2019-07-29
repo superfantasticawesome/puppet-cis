4c8cad837953c7985f9c9073bc8d5a525ac9ad96
superfantasticawesome <hello@superfantasticawesome.com>
2019-07-29 12:54:12 -0400

    Update aide.pp

--- manifests/aide.pp
+++ manifests/aide.pp
@@ -5,35 +5,35 @@ class cis::aide inherits cis::params {
   if ($cis::params::manage_aide) {
     case $::operatingsystem {
       'Ubuntu' : {
-        $aide_binary  = 'aide.wrapper'
-        $aide_init    = 'aideinit'
+        $aide_binary = 'aide.wrapper'
+        $aide_init   = 'aideinit'
       }
       'OracleLinux', 'RedHat', 'CentOS', 'Fedora', 'Amazon' : {
-        $aide_binary  = 'aide'
-        $aide_init    = 'aide --init'
+        $aide_binary = 'aide'
+        $aide_init   = 'aide --init'
       }
       default : {
-        $aide_binary  = 'aide'
-        $aide_init    = 'aide --init'
+        $aide_binary = 'aide'
+        $aide_init   = 'aide --init'
       }
     }
     
     package { 'aide': 
-      ensure          => 'installed', 
-      notify          => Exec["/usr/sbin/${aide_init}"],
+      ensure => 'installed', 
+      notify => Exec["/usr/sbin/${aide_init}"],
     } 
     
     cron { 'aide_watch':
-      command         => "/usr/sbin/${aide_binary} --check",
-      user            => root,
-      hour            => 5,
-      minute          => 0,
-      require         => Package['aide'],
+      command => "/usr/sbin/${aide_binary} --check",
+      user    => root,
+      hour    => 5,
+      minute  => 0,
+      require => Package['aide'],
     }
   
     exec { "/usr/sbin/${aide_init}":
-      refreshonly     => true,
-      timeout         => 0,
+      refreshonly => true,
+      timeout     => 0,
     }
     
     # Copy the newly initiailized db for RHEL-based systems
